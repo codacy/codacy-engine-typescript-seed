716ef5ee8311a044be6406c6eccd5ac78697899b
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-04-09 17:52:01 +0200

    clean: Refactor code in integration test

--- integration-tests/index.ts
+++ integration-tests/index.ts
@@ -4,31 +4,23 @@ const engine: Engine = async function (
   codacyrc?: Codacyrc
 ): Promise<CodacyResult[]> {
   function testResult() {
-    if (codacyrc == undefined) {
-      return [
-        new CodacyResult("file.js", "No .codacyrc", "no-codacyrc", 1),
-      ]
-    } else {
+    if (codacyrc) {
       const result: CodacyResult[] = []
       if (codacyrc.files) {
         result.concat(
           codacyrc.files.map(
-            (f: string) =>
-              new CodacyResult(f, "Found file", "found-file", 1)
+            (f: string) => new CodacyResult(f, "Found file", "found-file", 1)
           )
         )
       }
       if (codacyrc.tools && codacyrc.tools.length > 0) {
         result.push(
-          new CodacyResult(
-            "file.js",
-            codacyrc.tools[0].name,
-            "tool-name",
-            1
-          )
+          new CodacyResult("file.js", codacyrc.tools[0].name, "tool-name", 1)
         )
       }
       return result
+    } else {
+      return [new CodacyResult("file.js", "No .codacyrc", "no-codacyrc", 1)]
     }
   }
   return testResult()
